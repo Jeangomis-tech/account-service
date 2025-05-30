ffc77207f3712e778dcae87b6c72b22763cf159c
Jean Gomis <jeangomistech2018@gmail.com>
2025-05-30 21:15:04 +0200

    Create  AccountRepository class

--- src/main/java/com/jcg/account_service/repositories/AccountRepository.java
+++ src/main/java/com/jcg/account_service/repositories/AccountRepository.java
@@ -1,6 +1,6 @@
 package com.jcg.account_service.repositories;
 
-import com.jcg.account_service.domaines.Account;
+import com.jcg.account_service.domaines.account.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
