fe54fcc7c963d207b2cebf5e74612273b0f8aa81
Jean Gomis <jeangomistech2018@gmail.com>
2025-05-30 21:04:48 +0200

    Create CustomerClient interface

--- src/main/java/com/jcg/account_service/domaines/customer/CustomerClient.java
+++ src/main/java/com/jcg/account_service/domaines/customer/CustomerClient.java
@@ -1,4 +1,12 @@
 package com.jcg.account_service.domaines.customer;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "CUSTOMER_SERVICE", url = "${customer.url}")
 public interface CustomerClient {
+
+    @GetMapping("/customers/{id}")
+    Customer getCustomerById(@PathVariable Long id);
 }
