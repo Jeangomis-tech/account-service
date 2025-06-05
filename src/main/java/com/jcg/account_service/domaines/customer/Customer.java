39e87c979794c1d9de35d3a425ae16bbff30e744
Jean Gomis <jeangomistech2018@gmail.com>
2025-06-05 17:50:55 +0200

    modify Customer Class

--- src/main/java/com/jcg/account_service/domaines/customer/Customer.java
+++ src/main/java/com/jcg/account_service/domaines/customer/Customer.java
@@ -1,5 +1,12 @@
 package com.jcg.account_service.domaines.customer;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
 public class Customer {
     private String firstName;
     private String lastName;
