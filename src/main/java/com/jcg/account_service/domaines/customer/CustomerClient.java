9960a62a33f6d2044fca2f4086afb4ae1c17117a
Jean Gomis <jeangomistech2018@gmail.com>
2025-06-05 17:52:23 +0200

    modified CustomerClient

--- src/main/java/com/jcg/account_service/domaines/customer/CustomerClient.java
+++ src/main/java/com/jcg/account_service/domaines/customer/CustomerClient.java
@@ -1,12 +1,13 @@
 package com.jcg.account_service.domaines.customer;
 
+import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "CUSTOMER_SERVICE", url = "${customer.url}")
+@FeignClient(name = "CUSTOMER-SERVICE", url = "${customer.url}")
 public interface CustomerClient {
 
-    @GetMapping("/customers/{id}")
-    Customer getCustomerById(@PathVariable Long id);
+    @GetMapping("/{id}")
+    Customer getCustomerById(@Valid @PathVariable Long id);
 }
