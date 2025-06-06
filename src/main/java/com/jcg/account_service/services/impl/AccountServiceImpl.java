d08104430fccf5342cfb763d5d911e5a97c8a84f
Jean Gomis <jeangomistech2018@gmail.com>
2025-06-06 10:30:18 +0200

    Modified AccountServicImpe class

--- src/main/java/com/jcg/account_service/services/impl/AccountServiceImpl.java
+++ src/main/java/com/jcg/account_service/services/impl/AccountServiceImpl.java
@@ -19,11 +19,13 @@ public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
     private final AccountMapper accountMapper;
     private final CustomerClient client;
+    private final CustomerClient customerClient;
 
-    public AccountServiceImpl(AccountRepository accountRepository, AccountMapper accountMapper, CustomerClient client) {
+    public AccountServiceImpl(AccountRepository accountRepository, AccountMapper accountMapper, CustomerClient client, CustomerClient customerClient) {
         this.accountRepository = accountRepository;
         this.accountMapper = accountMapper;
         this.client = client;
+        this.customerClient = customerClient;
     }
 
     @Override
@@ -35,14 +37,19 @@ public class AccountServiceImpl implements AccountService {
         System.out.println(customer.getEmail() + " " + customer.getFirstName() + " " + customer.getLastName());
         Account account = accountMapper.toEntity(dto);
         account.setAccountNumber(generateAccountNumber());
-//        Account savedAccount = accountRepository.save(account);
-//        return savedAccount.getId();
-        return 1L;
+        account.setCustomerId(dto.customerId());
+        Account savedAccount = accountRepository.save(account);
+        return savedAccount.getId();
+//        return 1L;
 
     }
 
     @Override
     public Long updateAccount(Long id, AccountRequest dto) {
+        Customer customer = client.getCustomerById(dto.customerId());
+        if(customer == null) {
+            throw new IllegalArgumentException("Customer not found");
+        }
         Account account = accountMapper.toEntity(dto);
         account.setId(id);
         Account updatedAccount = accountRepository.save(account);
@@ -51,6 +58,7 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void deleteAccount(Long id) {
+        accountRepository.deleteById(id);
 
     }
 
@@ -63,7 +71,11 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public List<AccountResponse> getAllAccount() {
         List<Account> accounts = accountRepository.findAll();
-        return accounts.stream().map(accountMapper::toDto).toList();
+        return accounts.stream().map(account ->{
+            Customer customer = customerClient.getCustomerById(account.getCustomerId());
+            account.setCustomer(customer);
+        return accountMapper.toDto(account);
+        }).toList();
     }
     private String generateAccountNumber() {
         return "ACC" +  String.valueOf( System.currentTimeMillis()) +  UUID.randomUUID().toString().substring(0, 8);
